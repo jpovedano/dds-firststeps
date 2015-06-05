b3cfc720493e1ca8ff6c89378f19d02f2c7ce162
Javier Povedano <javier@rti.com>
2015-06-05 16:24:51 +0200

    Refactored sensor. Added timestamp

--- ex1-basic/sensor.py
+++ ex1-basic/sensor.py
@@ -5,20 +5,32 @@ import time
 import random
 
 
-connector = rti.Connector("MyParticipantLibrary::Sensor", 'Tutorial.xml')
+class Sensor(object):
+    def __init__(self, name):
+        self.id = name
+        self.value = 0
 
-writer = connector.getOutput("TempPublisher::TempWriter")
+    def readvalue(self):
+        # We simulate a sensor by generating random values
+        self.value = random.randint(0,100)
+        return self.value
+        
 
-
-sensorid = "SENSOR#0"
-
+if __name__ == '__main__':
     
-while 1:
-    sensorvalue = random.randint(0,100)
+    connector = rti.Connector("MyParticipantLibrary::Sensor", 'Tutorial.xml')
+    writer = connector.getOutput("TempPublisher::TempWriter")
 
-    writer.instance.setString('id', sensorid)
-    writer.instance.setNumber('value', sensorvalue)
-    print "Updating sensor {id} value: {value}".format(id=sensorid, value=sensorvalue)
-    writer.write()
-    time.sleep(1)
+    # Create a sensor
+    sensor = Sensor("SENSOR#0")
+        
+    while 1:
+        sensor.readvalue() 
+        writer.instance.setString('id', sensor.id)
+        writer.instance.setNumber('value', sensor.value)
+        writer.instance.setNumber('timestamp', time.time())
+
+        print "Updating sensor {id} value: {value}".format(id=sensor.id, value=sensor.value)
+        writer.write()
+        time.sleep(1)
 
